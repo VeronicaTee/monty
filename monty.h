bb57149bd6c5b940a987cdadd021cd57551a2091
Veronica <veronicatomilola@gmail.com>
2022-10-09 17:05:12 -0700

    Include header prototypes

--- monty.h
+++ monty.h
@@ -43,6 +43,16 @@ typedef struct instruction_s
 } instruction_t;
 
 
+/* PRIMARY INTERPRETER FUNCTIONS */
+void free_stack(stack_t **stack);
+int init_stack(stack_t **stack);
+int check_mode(stack_t *stack);
+void free_tokens(void);
+unsigned int token_arr_len(void);
+int run_monty(FILE *script_fd);
+void set_op_tok_error(int error_code);
+
+
 /* OPCODE FUNCTIONS */
 void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
